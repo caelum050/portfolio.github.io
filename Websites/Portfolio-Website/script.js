60a9dc8faa44d9c447e3bdc1355ad0d28b3aceb4
caelum050 <68352295+caelum050@users.noreply.github.com>
2020-10-05 19:40:42 -0500

    Portfolio-website

---
+++ Websites/Portfolio-Website/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    $('a[href*="#"]').click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  /*Animation scroll */
+
+  $('.js--wp-1').waypoint(function(direction) {
+      $('.js--wp-1').addClass('animate__animated animate__fadeInUp');
+  }, {
+      offset: '50%'
+  });
+
+})
