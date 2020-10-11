451f0d2dadc221275aeadf5f48b208b4fe0b4f25
caelum050 <68352295+caelum050@users.noreply.github.com>
2020-10-11 17:30:20 -0500

    presentation-page-updated

--- Websites/Portfolio-Website/script.js
+++ Websites/Portfolio-Website/script.js
@@ -1,42 +1,57 @@
-$(document).ready(function() {
+/*----------Side Navigation Menu----------*/
 
-    $('a[href*="#"]').click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
     }
-  });
+}
 
-  /*Animation scroll */
+showMenu('nav-toggle','nav-menu')
 
-  $('.js--wp-1').waypoint(function(direction) {
-      $('.js--wp-1').addClass('animate__animated animate__fadeInUp');
-  }, {
-      offset: '50%'
-  });
 
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const section = document.addEventListener.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    section.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=]' + sectionId + ']').classList.add('active')
+        }
+        else {
+            document.querySelector('.nav_menu a[href*=]' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
 })
+
+sr.reveal('.home_scroll', {delay: 200})
