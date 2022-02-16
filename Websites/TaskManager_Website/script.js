37f5c8ca21db0b576f5a886ae8a175405409760c
caelum050 <68352295+caelum050@users.noreply.github.com>
2022-02-16 14:56:13 -0500

    Add files via upload
    changes task manager app

--- Websites/TaskManager_Website/script.js
+++ Websites/TaskManager_Website/script.js
@@ -1,16 +1,15 @@
 function activity() {
-    var optionToCreate = document.getElementById("new-option").value;
-    var optionToAdd = document.getElementById("choose-activity");
+
     var value = document.getElementById("value").value;
     var currentDiv = document.getElementById("activity-container");
 
-    if(optionToCreate == "activity") {
+ 
         var checkDevExists = document.getElementById(value);
         if(!checkDevExists){
             var newDiv = document.createElement("DIV");
             var close = document.getElementsByClassName("closeAll");
             newDiv.setAttribute("id", value);
-            newDiv.setAttribute("class", "box");
+            newDiv.setAttribute("class", "board-list");
     
             var txt = document.createTextNode(value);
     
@@ -19,70 +18,67 @@ function activity() {
             newH3.setAttribute("align", "center");
     
             newDiv.appendChild(newH3);
-    
-            var newUL = document.createElement("UL");
-            newDiv.appendChild(newUL);
-            newUL.setAttribute("id", value + "list");
-    
-            currentDiv.appendChild(newDiv);
-            
-    
-            var option = document.createElement("OPTION");
-            option.value = value;
-            option.innerHTML = value;
-            optionToAdd.appendChild(option); 
 
-            var span = document.createElement("SPAN");
-            var txt = document.createTextNode("\u00D7");
-            span.className = "closeAll";
-            span.appendChild(txt);
-            newH3.appendChild(span);
-    
-            for (i = 0; i < close.length; i++) {
-                close[i].onclick = function() {
-                  var div = this.parentNode;
-                  div.parentNode.remove();
-                }
-            }
+            var addNewCard = document.createElement("DIV");
+            addNewCard.setAttribute("id", "add-new-card" + value);
+            addNewCard.setAttribute("class", "add-card");
+            addNewCard.innerHTML += '+ Add New Task';
+            addNewCard.addEventListener("click", task);
+            
+
+            newDiv.appendChild(addNewCard);
+
+            currentDiv.appendChild(newDiv);
+
         }
         else {
             alert("This activity already exist, use other name.");
         }
         
     }
-    else if (optionToCreate == "task")
-    {
-        var activityToAdd = document.getElementById("choose-activity").value;
-        var idToFind = activityToAdd + "list";
-        var ulToAdd = document.getElementById(idToFind);
-        var close = document.getElementsByClassName("close");
-        var checkDevExists = document.getElementById(value);
 
-        if(!checkDevExists){
-            var newLI = document.createElement("LI");
-            newLI.setAttribute("id", value);
-            ulToAdd.appendChild(newLI);
-            newLI.innerHTML = value;
     
-            var span = document.createElement("SPAN");
-            var txt = document.createTextNode("\u00D7");
-            span.className = "close";
-            span.appendChild(txt);
-            newLI.appendChild(span);
-    
-            for (i = 0; i < close.length; i++) {
-                close[i].onclick = function() {
-                  var div = this.parentNode;
-                  div.parentNode.removeChild(div);
-                }
-              }
-        }
-        else {
-            alert("This task already exist, please use other name");
-        }
+
+
+function task(clicked_id) {
+    var id = clicked_id.srcElement.attributes.id.value;
+    var parentId = document.getElementById(id).parentElement.id;
+
+    var close = document.getElementsByClassName("close");
+
+    var parentDiv = document.getElementById(parentId);
+
+    let text;
+    let newTask = prompt("Please enter new task:", "");
+    if(newTask == null || newTask == "") {
+        alert("Invalid task");
+    } else {
+        text = newTask;
+        var addCard = document.createElement("DIV");
+
+        addCard.setAttribute("class", "card");
+
+        var tag = document.createElement("p");
         
-        
-    
+        var txtT = document.createTextNode(text);
+        tag.appendChild(txtT);
+        addCard.appendChild(tag);
+        //addCard.innerHTML += newTask;
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+
+        addCard.appendChild(span);
+
+        parentDiv.appendChild(addCard);
+
+        for(i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                var div = this.parentNode;
+                div.parentNode.removeChild(div);
+            }
+        }
     }
-    
 }
